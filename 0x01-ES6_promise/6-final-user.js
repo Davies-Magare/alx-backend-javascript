8494175740c2b7343da871dcb87910d5376fd784
Davies Magare <davieskamanda@gmail.com>
2024-10-06 12:24:22 +0000

    Second attempt: Manually return array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => {
+      const resultArray = [];
+      for (const object of result) {
+        if ('status' in object && 'value' in object) {
+          resultArray.push({ status: `${object.status}`, value: `${object.value}` });
+        } else {
+          resultArray.push({ status: `${object.status}`, reason: `${object.reason}` });
+        }
+      }
+      return resultArray;
+    });
 }
