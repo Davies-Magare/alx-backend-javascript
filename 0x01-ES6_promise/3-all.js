ebd5a074bcf038bf63f1d16348e026119eef719c
Davies Magare <davieskamanda@gmail.com>
2024-10-06 09:46:57 +0000

    Task 3 second attempt

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,5 @@
 import createUser, { uploadPhoto } from './utils.js';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((values) => { console.log(values[0].body, values[1].firstName, values[1].lastName); }).catch(() => { console.log('Signup system offline'); });
+  return uploadPhoto().then((result) => createUser().then((result1) => { console.log(result.body, result1.firstName, result1.lastName); }).catch(() => { console.log('Signup system offline'); }));
 }
