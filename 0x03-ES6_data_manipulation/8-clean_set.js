4e227ed3f149dc8a9aeafd7b8a6c758ba8161ccb
Davies Magare <davieskamanda@gmail.com>
2024-10-11 20:46:16 +0000

    Task 8 corrections

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,8 @@
 export default function cleanSet(set, startString) {
   let resultString = '';
-  set.forEach((value) => {
-    if (value.startsWith(startString) && startString) {
-      resultString += `${value.substring(startString.length)}-`;
-    }
-  });
+  const setArray = Array.from(set);
+  for (let i = 0; i < setArray.length; i += 1) {
+    if (startString && typeof setArray[i] === 'string' && setArray[i].startsWith(startString)) resultString += `${setArray[i].substring(startString.length)}-`;
+  }
   return resultString.substring(0, resultString.length - 1);
 }
