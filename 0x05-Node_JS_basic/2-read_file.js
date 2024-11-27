8371ea90adc768e199c89dc78791b07d90737555
Davies Magare <davieskamanda@gmail.com>
2024-11-27 07:32:09 +0000

    Second attempt at task 2: correct code to reflect fields

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,30 +7,22 @@ const countStudents = function readFile(path) {
     }
     const parsedData = data.split('\n').filter((row) => row.length);
     const withoutField = parsedData.slice(1);
-    const cs = [];
-    const swe = [];
-    withoutField.forEach((student) => {
-      if (student.includes('CS')) {
-        cs.push(student);
-      } else {
-        swe.push(student);
-      }
+    const courses = {};
+    withoutField.slice().forEach((element) => {
+      const fieldArray = element.split(',');
+      const course = fieldArray.pop();
+      courses[course] = [];
     });
-    console.log(`Number of students: ${cs.length + swe.length}`);
-    function printer(list) {
-      let retStr = '';
-      for (let i = 0; i < list.length; i += 1) {
-        const name = list[i].split(',')[0];
-        if (i < list.length - 1) {
-          retStr += `${name}, `;
-        } else {
-          retStr += `${name}`;
-        }
-      }
-      return retStr;
-    }
-    console.log(`Number of students in CS: ${cs.length}. List: ${printer(cs)}`);
-    console.log(`Number of students in SWE: ${swe.length}. List: ${printer(swe)}`);
+    let totalStudents = 0;
+    withoutField.slice().forEach((element) => {
+      const fieldArray = element.split(',');
+      courses[fieldArray.pop()].push(fieldArray[0]);
+      totalStudents += 1;
+    });
+    console.log(`Number of students: ${totalStudents}`);
+    Object.keys(courses).forEach((key) => {
+      console.log(`Number of students in ${key}: ${courses[key].length}. List: ${courses[key].join(', ')}`);
+    });
   });
 };
 module.exports = countStudents;
