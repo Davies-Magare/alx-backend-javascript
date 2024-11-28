e8720769c83e098ebc2e236895a951c8dca833af
Davies Magare <davieskamanda@gmail.com>
2024-11-28 07:16:41 +0000

    Use task 3 countStudents for task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,28 +1,32 @@
 const fs = require('fs');
 
-const countStudents = function readFile(path) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
     const parsedData = data.split('\n').filter((row) => row.trim());
     const withoutField = parsedData.slice(1);
     const courses = {};
-    withoutField.slice().forEach((element) => {
-      const fieldArray = element.split(',');
-      const course = fieldArray.pop();
-      courses[course] = [];
-    });
     let totalStudents = 0;
-    withoutField.slice().forEach((element) => {
+
+    withoutField.forEach((element) => {
       const fieldArray = element.split(',');
-      courses[fieldArray.pop()].push(fieldArray[0]);
+      const last = fieldArray[fieldArray.length - 1];
+      if (!courses[last]) {
+        courses[last] = [];
+      }
+      courses[last].push(fieldArray[0]);
       totalStudents += 1;
     });
+
     console.log(`Number of students: ${totalStudents}`);
     Object.keys(courses).forEach((key) => {
-      console.log(`Number of students in ${key}: ${courses[key].length}. List: ${courses[key].join(', ')}`);
+      console.log(
+        `Number of students in ${key}: ${courses[key].length}. List: ${courses[key].join(', ')}`,
+      );
     });
-  });
-};
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
 module.exports = countStudents;
