88eab48347c71089b8d02b3ac08634b84ec41e3d
Davies Magare <davieskamanda@gmail.com>
2024-11-29 02:26:24 +0000

    Fix eslint problems

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const app = express();
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 app.listen(1245);
 
-module.exports = app
+module.exports = app;
